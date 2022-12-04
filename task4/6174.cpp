f432605fecae8042e166f186f24742706c703e98
Basmalla Abuhashim <99021600+BasmalaAbuhashim@users.noreply.github.com>
2022-12-05 00:44:37 +0200

    Add files via upload

---
+++ task4/6174.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+using namespace std;
+int main(){
+    string val;
+	int asc,des,count = 0;
+	cin>>val;
+	string diff = val;
+	while(diff != "6174"){
+	int x = 0 ;
+	count++;
+	sort(diff.begin(),diff.end());
+	asc = stoi(diff);
+	reverse(diff.begin(),diff.end());
+	des = stoi(diff);
+	if(des<1000){
+		des*=10;
+	}
+	if(des<100){
+		des*=100;
+	}
+	if(des<10){
+		des*=1000;
+	}
+	int maxi = max(asc , des);
+	int mini = min(asc , des);
+	x = maxi - mini ; 
+	diff = to_string(x);
+	}
+	cout<<count;
+
+}
